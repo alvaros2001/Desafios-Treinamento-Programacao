fa10fa87a9c5da24e57f67bd43b26a89b4c824ef
alvaros2001 <80852758+alvaros2001@users.noreply.github.com>
2025-07-28 11:18:44 -0300

    Implementando lógica carrinho de compras

---
+++ 3411-praticando-logica-projeto_inicial/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -0,0 +1,32 @@
+let totalGeral = 0
+
+function adicionar() {
+    let quantidade = document.getElementById('quantidade').value;
+    let produto = document.getElementById('produto').value;
+
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+
+    let preco = quantidade * valorUnitario;
+
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`
+
+    totalGeral = totalGeral + preco
+
+    let valorTotal = document.getElementById('valor-total');
+    valorTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${totalGeral}</span>`;
+
+
+}
+
+function limpar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('valor-total').innerHTML = `<span class="texto-azul" id="valor-total">R$0,00</span>`
+    document.getElementById('lista-produtos').innerHTML = '';
+    totalGeral = 0;
+
+
+}
